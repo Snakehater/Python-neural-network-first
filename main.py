3616288eff0feff435c543e7e285fbe4d7bc6587
Vigor Turujlija <vigor.turujlija@gmail.com>
2019-12-23 01:07:19 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+def sigmoid_deritive(x):
+    return x * (1 - x)
+
+training_inputs = np.array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+
+training_outputs = np.array([[0, 1, 1, 0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random.random((3, 1)) - 1
+print('synaptic_weights: ')
+print(synaptic_weights)
+print('\n')
+
+for iteration in range(20000):
+
+    input_layer = training_inputs
+
+    outputs = sigmoid(np.dot(input_layer, synaptic_weights ))
+
+    error = training_outputs - outputs
+
+    adjustments = error - sigmoid_deritive(outputs)
+
+    synaptic_weights += np.dot(input_layer.T, adjustments)
+
+print('synaptic_weights after training: ')
+print(synaptic_weights)
+
+print('Output after training: ')
+print(outputs)
